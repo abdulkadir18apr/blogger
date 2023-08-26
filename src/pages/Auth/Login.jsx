279dfeed90336e84db61106f83e587479ccb7999
abdulkadir18apr <114944605+abdulkadir18apr@users.noreply.github.com>
2023-08-26 12:22:09 +0530

    --validationChecks

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -7,7 +7,47 @@ import "./login.css"
 
 export default function Login() {
 
-  const [isSignup,setSignUp]=useState(false)
+  const [isSignup,setSignUp]=useState(false);
+  const [isValidEmail,setIsValidEmail]=useState(false);
+  const [isvalidPassword,setIsvalidPassword]=useState(false)
+  const [invalidMsg,setInvalidMsg]=useState({email:"",password:""})
+
+
+  const validateEmail = (email) => {
+    const pattern = /^[a-zA-Z0-9._%+-]+@nucleustech\.com$/;
+    return pattern.test(email);
+  };
+
+
+
+  const handleEmailChange = (e) => {
+    const email = e.target.value;
+    const isValid = validateEmail(email);
+    isValid?setIsValidEmail(true):setIsValidEmail(false)
+
+
+  };
+const showValidateMsg=(feild,msg)=>{
+  setInvalidMsg(()=>({...invalidMsg,[feild]:msg}));
+  }
+
+  const handlePasswordChange=(e)=>{
+    if(e.target.value.length<8){
+      setIsvalidPassword(true)
+      setInvalidMsg(()=>({...invalidMsg,password:'minimum length of password must be 8'}))
+    }
+    else{
+      setIsvalidPassword(false)
+      setInvalidMsg(()=>({...invalidMsg,password:''}))
+
+    }
+  }
+
+
+
+ 
+
+
   return (
     <div className='login'>
       <div className="login-container">
@@ -27,13 +67,18 @@ export default function Login() {
 
           </div>}
           <div className="form-element">
-          <label htmlFor="email" className='starlabel' >Email</label>
-          <input type="text" id="email" name="email" placeholder='abc@gmail.com' />
-
+          <label htmlFor="email" className='starlabel'  >Email</label>
+          <input type="text" id="email" name="email" placeholder='abc@nucleustech.com' onChange={handleEmailChange} onBlur={()=>showValidateMsg("email",isValidEmail?"":"please enter company email")} />
+          {
+              invalidMsg.email.length!==0 && <p style={{color:"red",marginBlockStart:0,marginBlockEnd:0}}>{invalidMsg.email}</p>
+          }
           </div>
           <div className="form-element">
           <label htmlFor="password" className='starlabel'>Password</label>
-          <input type="password" name="password" id="password" />
+          <input type="password" name="password" id="password" onChange={handlePasswordChange} />
+          {
+              invalidMsg.password.length!==0 && <p style={{color:"red",marginBlockStart:0,marginBlockEnd:0}}>{invalidMsg.password}</p>
+          }
 
           {!isSignup && <div className="form-element forgot-password">
             <h5>forgot password?</h5>
@@ -47,7 +92,7 @@ export default function Login() {
 
 
           <div className="form-element">
-            <button className='primary-btn'>{isSignup?"Signup":"Login"}</button>
+            <button className='primary-btn' disabled={(isValidEmail && isvalidPassword)} >{isSignup?"Signup":"Login"}</button>
           </div>
         </form>
         <p>{!isSignup?"New here ? ":"Already have a account ?"} <a href='#' onClick={()=>setSignUp(()=>!isSignup)} >{isSignup?"signup":"login"}</a></p>
